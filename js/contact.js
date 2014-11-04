7fda94a30eafebf50edb577192bbe2f439192819
gushterche <gushterche_2006@abv.bg>
2014-11-04 16:34:33 +0200

    page finished without SEO and ANALYTICS

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,19 @@
 	function initialize() {
-        var map_canvas = document.getElementById('map_canvas');
-        var map_options = {
-          center: new google.maps.LatLng(42.492381, 27.47954),
+        var mapCanvas = document.getElementById('map_canvas');
+        var mapOptions = {
+          center: new google.maps.LatLng(42.6950698, 23.3400025),
           zoom: 17,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-        var map = new google.maps.Map(map_canvas, map_options)
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+
+        var image = '../img/pointer.png';
+  var myLatLng = new google.maps.LatLng(42.6950698, 23.3400025);
+  var beachMarker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      icon: image
+  });
       	}
       	
 
